355c50144496b5e043bb7aafb031f16f7544a15f
Shanilka Liyanage <shanilka.liyanage@pronto.net>
2022-10-16 11:52:43 +0530

    Flatlist code changes

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 import {AppRegistry} from 'react-native';
 // import App from './App';
 import {name as appName} from './app.json';
-import Lesson_01 from './src/Lesson_01';
-// import Lesson_02 from './src/Lesson_02';
+import Lesson_02 from './src/Lesson_02';
 
-AppRegistry.registerComponent(appName, () => Lesson_01);
+AppRegistry.registerComponent(appName, () => Lesson_02);
--- src/Lesson_02.js
+++ src/Lesson_02.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, StatusBar} from 'react-native';
+import {
+  SafeAreaView,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+
+const DATA = [
+  {
+    id: 1,
+    title: 'First Item',
+  },
+  {
+    id: 2,
+    title: 'Second Item',
+  },
+  {
+    id: 3,
+    title: 'Third Item',
+  },
+];
 
 const Lesson_02 = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Lesson 02</Text>
+      <FlatList
+        data={DATA}
+        renderItem={({item}) => (
+          <TouchableOpacity style={styles.item}>
+            <Text style={styles.title}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item.id}
+      />
     </SafeAreaView>
   );
 };
@@ -13,12 +43,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
+  item: {
+    backgroundColor: 'white',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
 
+    // ==================
+
+    elevation: 9, // for android
+
+    //for ios
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    borderRadius: 8,
+    //
+  },
   title: {
-    fontSize: 20,
+    fontSize: 32,
   },
 });
 
