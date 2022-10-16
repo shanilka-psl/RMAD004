8b73b7b07aaa9696f607ab7a46d38e7bc1c35370
Shanilka Liyanage <shanilka.liyanage@pronto.net>
2022-10-16 11:14:41 +0530

    updated code

--- src/Lesson_01.js
+++ src/Lesson_01.js
@@ -57,6 +57,19 @@ const Lesson_01 = () => {
     // 55 - 65 -> C
     // 35 - 55 -> s
     // 35 - W
+
+    if (marks >= 75) {
+      console.log('A');
+    } else if (marks >= 65) {
+      console.log('B');
+    } else if (marks >= 55) {
+      console.log('c');
+    } else if (marks >= 35) {
+      console.log('S');
+    } else {
+      console.log('W');
+      console.log('W');
+    }
   };
 
   return (
